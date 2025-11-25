453cbf1dfe082678f39495fd4d1df8e55e8c7382
ABHINESH <22n201@psgtech.ac.in>
2025-11-25 17:13:27 +0530

    URL updates

--- client/groups.js
+++ client/groups.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://krp-expense-backend.onrender.com/api';
 
 const groupListEl = document.getElementById('groupList');
 const createCardEl = document.getElementById('createGroupCard');
